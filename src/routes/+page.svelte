c4c9d9deb511e6ca3a1cd05f2450409bef0fde85
Shidorien <Gaetangerard@hotmail.fr>
2024-03-05 22:10:57 +0100

    clear template

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,4 @@
 <h1 class="text-3xl font-bold underline">
     Hello world!
-  </h1>
+</h1>
   
-  <style lang="postcss">
-    :global(html) {
-      background-color: theme(colors.gray.100);
-    }
-  </style>
