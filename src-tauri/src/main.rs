50a8bf1c4fd38e72ef8f124094cb92e9e6d3240d
Shidorien <Gaetangerard@hotmail.fr>
2024-03-14 00:14:49 +0100

    Add create_default_profil command

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,21 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+mod data;
+mod keyboard_layout;
+
+use data::ProfilGame;
+
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![create_default_profil])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
+
+#[tauri::command]
+fn create_default_profil() -> ProfilGame {
+    println!("create_default_profil");
+    let profil = ProfilGame::new("test Game");
+    profil
 }
