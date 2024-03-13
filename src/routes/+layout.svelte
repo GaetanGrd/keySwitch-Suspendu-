ca7c8cb64f2f03fcce51a96a70096d8318d14090
Shidorien <Gaetangerard@hotmail.fr>
2024-03-14 00:14:33 +0100

    Add createProfile function and button to layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,19 @@
 <script>    
 	import './styles.css';
+    import { invoke } from '@tauri-apps/api/tauri'
+
+    async function createProfile() {
+        const result = await invoke('create_default_profil')
+        console.log(result)
+    }
 </script>
 
 <div class="app">
 	<main>
+        <div>
+            <button on:click={createProfile}>Create Profile</button>
+        </div>
+        
 		<slot />
 	</main>
 </div>
