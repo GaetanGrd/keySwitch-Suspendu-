7ff5abe46cd244a2261ca9c7c5ac05ea8238bac7
shidorien <gaetangerard@hotmail.fr>
2024-04-10 18:44:43 +0200

    Update keyboard_layout to KeyboardLayoutInfo in ProfilGame struct

--- src-tauri/src/data.rs
+++ src-tauri/src/data.rs
@@ -1,28 +1,26 @@
-use crate::keyboard_layout::KeyboardLayout;
+use crate::keyboard_layout::KeyboardLayoutInfo;
 use serde::{Serialize, Deserialize};
 
 #[derive(Serialize, Deserialize)]
 pub struct ProfilGame {
     pub name: String,
-    pub keyboard_layout: KeyboardLayout
+    pub keyboard_layout: KeyboardLayoutInfo
 }
 
 impl ProfilGame {
     pub fn new(name: &str) -> ProfilGame {
         ProfilGame {
             name: name.to_string(),
-            keyboard_layout: KeyboardLayout::new()
+            keyboard_layout: KeyboardLayoutInfo::new().expect("Failed to get keyboard layout info")
         }
     }
 }
-
 
 impl Default for ProfilGame {
     fn default() -> Self {
         ProfilGame {
             name: String::new(),
-            keyboard_layout: KeyboardLayout::new()
+            keyboard_layout: KeyboardLayoutInfo::new().expect("Failed to get keyboard layout info")
         }
     }
 }
-
